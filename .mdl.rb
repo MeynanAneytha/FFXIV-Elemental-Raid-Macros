c4e804741966462ef15babc1dad959202eb8dc43
MeynanAneytha <meynan@proton.me>
2023-06-05 13:34:49 +0200

    migrate current build system to gh actions

---
+++ .mdl.rb
@@ -0,0 +1,13 @@
+# Enable all rules by default
+all
+
+# Setting limit at 900 because of waymarks info
+rule 'MD013', :line_length => 900
+
+# Allow in-line HTML - easier right now than specifying 
+# each and every authorized tag
+exclude_rule 'MD033'
+
+# Nested lists should be indented with four spaces.
+rule 'MD007', :indent => 2
+
